a6c9ce441861776e53d1ae4f43ee5ca3e78f455e
Carlo Manalese <CarloManalese@calvisitor-10-105-148-48.calvisitor.1918.berkeley.edu>
2018-01-27 16:06:55 -0800

    fruits TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+var triviaQuestions = [
+{
+	question: ["Eyes benefits from which vegetable?"],
+	options: ["Eggplant", "Zucchini", "Carrots", "Squash"],
+	answer: "Carrots"
+},
+
+{
+	question: ["Which vegetable has been linked to many health benefits such as reduced heart disease and cancer?"],
+	options: ["Cabbage", "Celery", "Brussel sprout", "Tomato"],
+	answer: "Tomato"
+},
+
+{
+	question: ["This fruit can be helpful in overcoming depression due to high levels of tryptophan which the body converts to serotonin, the mood-elevating brain neurotransmitter. This fruit is also rich in potassium."],
+	options: ["Apple", "Banana", "Grapes", "Orange"],
+	answer: "Banana"
+},
+
+{
+	question: ["What fruit can you eat everyday to keep the doctor away?"],
+	options: ["Pears", "Pineapple", "Strawberry", "Apple"],
+	answer: "Apple"
+}
+]
+
+function myFunction(){
+	alert("sup");
+}
+
+console.log(triviaQuestions[0].question);
+console.log(triviaQuestions[0].options);
+console.log(triviaQuestions[0].answer);
+
+console.log(triviaQuestions[1].question);
+console.log(triviaQuestions[1].options);
+console.log(triviaQuestions[1].answer);
+
+console.log(triviaQuestions[2].question);
+console.log(triviaQuestions[2].options);
+console.log(triviaQuestions[2].answer);
+
+console.log(triviaQuestions[3].question)
+console.log(triviaQuestions[3].options)
+console.log(triviaQuestions[3].answer)
